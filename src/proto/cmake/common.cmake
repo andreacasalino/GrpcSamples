2a47cfef2dc6e76f904afcad3cbdf1f3495a5855
Foo <foo:gmail.com>
2024-06-10 21:51:47 +0000

    impl

--- src/proto/cmake/common.cmake
+++ src/proto/cmake/common.cmake
@@ -1,22 +1,3 @@
-# Copyright 2018 gRPC authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# cmake build file for C++ route_guide example.
-# Assumes protobuf and gRPC have been installed using cmake.
-# See cmake_externalproject/CMakeLists.txt for all-in-one cmake build
-# that automatically builds all the dependencies before building route_guide.
-
 cmake_minimum_required(VERSION 3.8)
 
 if(MSVC)
@@ -102,11 +83,11 @@ else()
 
   set(_PROTOBUF_LIBPROTOBUF protobuf::libprotobuf)
   set(_REFLECTION gRPC::grpc++_reflection)
-  if(CMAKE_CROSSCOMPILING)
+  # if(CMAKE_CROSSCOMPILING)
     find_program(_PROTOBUF_PROTOC protoc)
-  else()
-    set(_PROTOBUF_PROTOC $<TARGET_FILE:protobuf::protoc>)
-  endif()
+  # else()
+  #   set(_PROTOBUF_PROTOC $<TARGET_FILE:protobuf::protoc>)
+  # endif()
 
   # Find gRPC installation
   # Looks for gRPCConfig.cmake file installed by gRPC's cmake installation.
@@ -114,9 +95,9 @@ else()
   message(STATUS "Using gRPC ${gRPC_VERSION}")
 
   set(_GRPC_GRPCPP gRPC::grpc++)
-  if(CMAKE_CROSSCOMPILING)
+  # if(CMAKE_CROSSCOMPILING)
     find_program(_GRPC_CPP_PLUGIN_EXECUTABLE grpc_cpp_plugin)
-  else()
-    set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:gRPC::grpc_cpp_plugin>)
-  endif()
+  # else()
+  #   set(_GRPC_CPP_PLUGIN_EXECUTABLE $<TARGET_FILE:gRPC::grpc_cpp_plugin>)
+  # endif()
 endif()
