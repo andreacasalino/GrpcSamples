d74ef3ec57179bbc55a37b891b397b86c6643e80
Foo <foo:gmail.com>
2024-06-11 11:34:22 +0000

    impl

--- src/app/orderBook/src/tests/OrderBookTest.cpp
+++ src/app/orderBook/src/tests/OrderBookTest.cpp
@@ -2,22 +2,41 @@
 
 #include <OrderBook.h>
 
-using namespace book;
+namespace book::testing {
+using TOB = std::pair<OrderBook::PriceQuantity, srv::Ids>;
+using HistoryStep = std::tuple<std::optional<TOB>, std::optional<TOB>, std::optional<srv::Trade>>;
+using History = std::vector<HistoryStep>;
 
 struct OrderBookHistory {
     void operator()(srv::Trade&& trade) const;
+
+    History history;
 };
 
 struct OrderBookFixture : ::testing::Test {
-    void SetUp() override {}
+    void setUpExpectedHistory(std::vector<HistoryStep>&& expected);
 
+    void newOrder(const srv::NewOrderRequest& order);
+
+    void cancelOrder(const srv::Ids& ids);
+
+    void TearDown() override;
+
+private:
+    template<typename Pred>
+    void useBook_(Pred pred);
 
     OrderBookHistory history;
     OrderBook book{[this](srv::Trade&& trade){
         history(std::forward<srv::Trade>(trade));
     }};
 };
+}
 
-TEST_F(OrderBookFixture, one_order) {
+TEST_F(book::testing::OrderBookFixture, one_order) {
+    // TODO set up expected history
 
+    // TODO push orders cancels
+    // TODO push orders cancels
+    // TODO push orders cancels
 }
