dfbe38622d40b5fed1f7b9a15dcafdd0773f2ccd
Foo <foo:gmail.com>
2024-06-11 08:27:48 +0000

    impl

--- src/app/FooServer/Client.cpp
+++ src/app/FooServer/Client.cpp
@@ -3,44 +3,32 @@
 #include <FooService.grpc.pb.h>
 
 class GreeterClient {
- public:
+public:
   GreeterClient(std::shared_ptr<::grpc::Channel> channel)
       : stub_(srv::FooService::NewStub(channel)) {}
 
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-//   std::string SayHello(const std::string& user) {
-//     // Data we are sending to the server.
-//     HelloRequest request;
-//     request.set_name(user);
+  std::string request() {
+    srv::FooRequest request;
+    request.set_name("Pinco");
+    srv::FooResponse response;
+    ::grpc::ClientContext ctxt;
 
-//     // Container for the data we expect from the server.
-//     HelloReply reply;
+    ::grpc::Status status = stub_->GiveMeAFoo(&ctxt, request, &response);
 
-//     // Context for the client. It could be used to convey extra information to
-//     // the server and/or tweak certain RPC behaviors.
-//     ClientContext context;
+    if (!status.ok()) {
+      throw std::runtime_error{"Failed"};
+    }
+    return response.mutable_payload()->name();
+  }
 
-//     // The actual RPC.
-//     Status status = stub_->SayHello(&context, request, &reply);
-
-//     // Act upon its status.
-//     if (status.ok()) {
-//       return reply.message();
-//     } else {
-//       std::cout << status.error_code() << ": " << status.error_message()
-//                 << std::endl;
-//       return "RPC failed";
-//     }
-//   }
-
- private:
+private:
   std::unique_ptr<srv::FooService::Stub> stub_;
 };
 
 int main() {
-    GreeterClient client{::grpc::CreateChannel("TODO server address", grpc::InsecureChannelCredentials())};
+  GreeterClient client{::grpc::CreateChannel("0.0.0.0:50051", grpc::InsecureChannelCredentials())};
 
+  std::cout << client.request() << std::endl;
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
